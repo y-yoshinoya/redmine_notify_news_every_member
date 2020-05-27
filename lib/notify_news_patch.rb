c035f231b516a034d21924f8c8d8389819ac592a
y-yoshinoya <y-yoshinoya@ase.co.jp>
2020-05-27 17:34:04 +0900

    Compatible with Redmine 4.1+

--- lib/notify_news_patch.rb
+++ lib/notify_news_patch.rb
@@ -1,36 +1,14 @@
 require_dependency 'news'
 
 module NotifyNewsPatch
-  def self.included(base)
-    base.send(:include, WrapperMethods)
-
-    base.class_eval do
-      alias_method_chain :notified_users, :patch
-      alias_method_chain :send_notification, :patch
-    end
-  end
-
-  module WrapperMethods
-    def notified_users_with_patch
-      notified = project.users  # original: project.notified_users
-      notified.select {|user| visible?(user)}
-    end
-
-    def send_notification_with_patch
-      if Setting.notified_events.include?('news_added')
-        mail = Mailer.news_added(self)
-        all_addresses = mail.to + mail.cc + mail.bcc
-        all_addresses.uniq.each_slice(50).each.with_index do |addresses, index|
-          m = mail.dup
-          m.message_id = m.message_id.gsub(/@/, "#{index}@")
-          m.to = []
-          m.cc = []
-          m.bcc = addresses
-          m.deliver
-        end
-      end
-    end
+  def notified_users
+    notified = project.users
+    notified.select {|user| visible?(user)}
   end
 end
 
-News.send(:include, NotifyNewsPatch)
+ActiveSupport::Reloader.to_prepare do
+  unless News.included_modules.include?(NotifyNewsPatch)
+    News.prepend(NotifyNewsPatch)
+  end
+end
